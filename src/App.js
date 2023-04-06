362030fade663af333cc776accbdacdcca96e2fc
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-06 17:00:22 +0530

    aman cameback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Home from './components/Home'
-import JobDetails from './components/Job-Details/JobDetails';
+import JobDetails from './components/job-details/JobDetails';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import AddJob from './components/AddJob';
-import Sidebar from './components/Sidebar';
+// import Sidebar from './components/Sidebar';
 
 const router = createBrowserRouter([
   {
