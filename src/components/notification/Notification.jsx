a0f9152b0fa27158a22d543500d126d03a73ed9f
anmolchhabra21 <anmolchhabra71500@gmail.com>
2023-04-08 13:48:26 +0530

    notifiactions done2

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -8,50 +8,49 @@ const Notification = () => {
   let fetchcompname = "";
   let fdata = {};
 
-  const studentnot = async() =>{
+  const studentnot = async () => {
     console.log("usernames ", usernames);
-    for(let i=0; i<usernames.length; i++){
+    for (let i = 0; i < usernames.length; i++) {
       const docRef = doc(db, "student", usernames[i]);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data2:", docSnap.data());
+        let arr = [];
+        if (docSnap.data().messageCompany) {
+          arr = docSnap.data().messageCompany;
+        }
+        fdata.msgcomp = fetchcompname;
+        arr.push(fdata);
+        console.log("fdata is", fdata);
+        setDoc(docRef, { messageCompany: arr }, { merge: true });
+        console.log("added");
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document2!");
+      }
+    }
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    // setFdata({"desc": data.get("textarea"), "title": data.get("title")});
+    fdata = { desc: data.get("textarea"), title: data.get("title") };
+
+    console.log("data ", data.get("textarea"));
+
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        // console.log("user", user.);
+        const docRef = doc(db, "companies", user.uid);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-          console.log("Document data2:", docSnap.data());
-          let arr = [];
-          if(docSnap.data().messageCompany){
-            arr = docSnap.data().messageCompany;         
-          }
-          fdata.msgcomp =  fetchcompname;
-          arr.push(fdata);
-          console.log("fdata is",fdata);
-          setDoc(docRef, { messageCompany : arr }, { merge: true });
-          console.log("added")
-        } else {
-          // docSnap.data() will be undefined in this case
-          console.log("No such document2!");
-        }
-      }
-      
-    }
-    const handleClick = async (e) => {
-      e.preventDefault();
-      const data = new FormData(e.currentTarget);
-      // setFdata({"desc": data.get("textarea"), "title": data.get("title")});
-      fdata = {"desc": data.get("textarea"), "title": data.get("title")};
-      
-      console.log("data ", data.get("textarea"));
-      
-      auth.onAuthStateChanged(async (user) => {
-        if (user) {
-          // console.log("user", user.);
-          const docRef = doc(db, "companies", user.uid);
-          const docSnap = await getDoc(docRef);
-          
-          if (docSnap.exists()) {
-            // console.log("Document data:", docSnap.data().studentsApplied);
-            fetchcompname = docSnap.data().companyName;
-            console.log("companyName", fetchcompname);
-            usernames = docSnap.data().studentsApplied;
-            await studentnot();
+          // console.log("Document data:", docSnap.data().studentsApplied);
+          fetchcompname = docSnap.data().companyName;
+          console.log("companyName", fetchcompname);
+          usernames = docSnap.data().studentsApplied;
+          await studentnot();
         } else {
           // docSnap.data() will be undefined in this case
           console.log("No such document!");
