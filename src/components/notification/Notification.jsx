54cb13f48b57873c9939f0c79f7abb8b8b36183e
amanbhardwaj12072003 <amanbhardwaj12072003@gmail.com>
2023-04-08 10:43:13 +0530

    Company side notificaion page added

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -3,28 +3,23 @@ import aman from "./Notification.module.css";
 
 const Notification = () => {
   return (
-    <div className={aman.container}>
-      <div className={aman.notificationWrapper}>
-        <form className={aman.formWrapper} action="#">
-          <h2 className="heading">Notify Placement Cell</h2>
+    <div className={aman.formWrapper}>
+      <form className={aman.form} action="#">
+        <h2 className={aman.heading}>Notify Placement Cell</h2>
 
-          
-            <input
-              className={aman.input}
-              type="text"
-              placeholder="Company Name"
-            />
-            <input
-              className={aman.input}
-              type="text"
-              placeholder="Job Role"
-            />
-          <textarea className={aman.input} name="" id="" cols="30" rows="5">
-            Enter the decscription about the hiring...
-          </textarea>
-          <button className="sendButton">Send Notification</button>
-        </form>
-      </div>
+        <input className={aman.input} type="text" placeholder="Company Name" />
+        <input className={aman.input} type="text" placeholder="Job Role" />
+        <textarea
+        style={{resize:'none'}}
+          className={aman.input}
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Enter the decscription about the hiring..."
+        ></textarea>
+        <button className={aman.sendButton}>Send Notification</button>
+      </form>
     </div>
   );
 };
---
+++ src/components/notification/StudentNotification.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StudentNotification = () => {
+  return (
+    <div>
+      <h1>Student Notifications</h1>
+    </div>
+  );
+};
+
+export default StudentNotification;
